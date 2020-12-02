800c5985e92cdc24aeadc9d438f0d62437bf7a90
Ben Winding <hello@benwinding.com>
2020-12-03 01:13:13 +1030

    Add ts declaration to build

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,26 +1,30 @@
 import { Component as NgComponent, NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
+// import { RouterModule } from "@angular/router";
 
 type AngularModule = any;
+type AngularComponent = any;
 
-export function Component(input: { template: string; imports: AngularModule[] }) {
+export interface PageComponentProps {
+  layout?: AngularComponent;
+  template: string;
+  imports?: AngularModule[];
+}
+
+export function Component(input: PageComponentProps) {
   return function (constructorFunction: Function) {
-    const { template, imports } = input;
-
-    @NgComponent({
-      template: template,
-    })
-    class PageComponent {}
-
-    @NgModule({
-      imports: [
-        ...imports,
-        RouterModule.forChild([{ path: "**", component: PageComponent }]),
-      ],
-      declarations: [PageComponent],
-    })
-    class RoutingModule {}
-
-    return RoutingModule;
+    // const { template, imports } = input;
+    // @NgComponent({
+    //   template: template,
+    // })
+    // class PageComponent {}
+    // @NgModule({
+    //   imports: [
+    //     ...imports,
+    //     RouterModule.forChild([{ path: "**", component: PageComponent }]),
+    //   ],
+    //   declarations: [PageComponent],
+    // })
+    // class RoutingModule {}
+    // return RoutingModule;
   };
 }
