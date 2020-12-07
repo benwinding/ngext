90f580ab1e4dc9a8b2e6b3712a5aeda733b0c08a
Ben Winding <hello@benwinding.com>
2020-12-07 17:07:35 +1030

    Fix typings

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,16 +1,18 @@
 import { Component as NgComponent, NgModule } from "@angular/core";
 // import { RouterModule } from "@angular/router";
 
-type AngularModule<T> = new () => T;
-type AngularComponent<T> = new () => T;
+type AngularModule = any;
+type AngularComponent = any;
 
-export interface PageComponentProps<T> {
-  layout?: AngularComponent<T>;
+export interface Type<T> extends Function { new (...args: any[]): T; }
+
+export interface PageComponentProps {
+  layout?: Type<AngularComponent>;
   template: string;
-  imports?: AngularModule<T>[];
+  imports?: Type<AngularModule>[];
 }
 
-export function Component(input: PageComponentProps<any>) {
+export function Component(input: PageComponentProps) {
   return function (constructorFunction: Function) {
     // const { template, imports } = input;
     // @NgComponent({
